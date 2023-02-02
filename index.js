0ca3a2511c26b9ee040e63d4ca6f6688806150db
ianstrom <iankstrom@gmail.com>
2023-02-01 22:49:01 -0500

    finishing labs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(data => {return data.json()
+})
+.then(data => renderBooks(data))
 }
 
 function renderBooks(books) {
